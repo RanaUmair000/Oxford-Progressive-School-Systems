5291392d4bf342201efead5c4b2d4a3d01660c69
M Umair <ranaumairfyp@gmail.com>
2026-04-08 00:37:07 +0500

    f

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,11 @@ const morgan = require('morgan');
 
 dotenv.config();
 const app = express();
-app.use(helmet());
-
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false, // ✅ disable default blocking
+  })
+);
 const courseRoutes = require("./routes/courseRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const classRoutes = require("./routes/classRoutes");
@@ -22,6 +25,7 @@ const authRoutes = require('./routes/authRoutes');
 const stockRoutes = require('./routes/stockRoutes');
 const diaryRoutes = require("./routes/diaryRoutes");
 const teacherAttendanceRoutes = require('./routes/teacherAttendanceRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
 
 
 // Connect to database
@@ -32,7 +36,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"], // frontend URL
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://oxfordprogressiveschool.vercel.app"], // frontend URL
   methods: ["GET","POST","PUT","DELETE", "PATCH"],
 }));
 app.use(express.json({ limit: '10mb' }));
@@ -53,9 +57,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/stock', stockRoutes);
 app.use("/api/diary", diaryRoutes);
 app.use('/api/teacher-attendance', teacherAttendanceRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
